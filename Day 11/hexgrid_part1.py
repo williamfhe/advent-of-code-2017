16f0381d41a2f85a6bd26ce341e8632f0e2d3b83
williamfhe <william-frederic.hess@hotmail.fr>
2017-12-11 21:50:34 +0100

    Day 11

---
+++ Day 11/hexgrid_part1.py
@@ -0,0 +1,23 @@
+
+dirs = {
+    "n": (0, +1, -1),
+    "nw": (-1, 1, 0),
+    "ne": (1, 0, -1),
+    "se": (1, -1, 0),
+    "s": (0, -1, 1),
+    "sw": (-1, 0, 1)
+}
+
+with open("input.txt") as f:
+    entry = f.readline().split(',')
+
+x, y, z = 0, 0, 0
+
+for move in entry:
+    xx, yy, zz = dirs[move]
+    x += xx
+    y += yy
+    z += zz
+
+steps = (abs(x) + abs(y) + abs(z)) / 2
+print(steps)
